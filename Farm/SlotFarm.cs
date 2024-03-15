6aa0fa0023cbd43f97d9fcf44e0836d8183f8e73
AndreLuiz-LC <andrelzlemosc@gmail.com>
2024-03-15 14:46:27 -0300

    Pasta deletada "Projeto gamedev 2D"

---
+++ Farm/SlotFarm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotFarm : MonoBehaviour
+{
+    [SerializeField] Sprite hole;
+    [SerializeField] Sprite carrot;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    { 
+        
+    }
+}
