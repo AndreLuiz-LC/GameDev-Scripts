006c1047417fd0632c75fbf6245c8c2a1ef2370c
AndreLuiz-LC <andrelzlemosc@gmail.com>
2024-07-01 11:24:02 -0300

    Lógica de construção - casa

---
+++ Buildings/House.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    [Header("Amounts")]
+    [SerializeField] private int woodAmount;
+    [SerializeField] private float timeAmount;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+
+    [Header("Components")]
+    [SerializeField] private GameObject houseColl;
+    [SerializeField] private SpriteRenderer houseSprite;
+    [SerializeField] private Transform point;
+
+
+
+    private bool detectingPlayer;
+    private Player player;
+    private PlayerAnim playerAnim;
+    private PlayerItems playerItems;
+    
+    private float timeCount;
+    private bool isBeginning; 
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        playerAnim= player.GetComponent<PlayerAnim>();
+        playerItems = player.GetComponent<PlayerItems>();
+    }
+
+    void Update()
+    {
+        if (detectingPlayer && Input.GetKeyDown(KeyCode.E) && playerItems.currentWood >= woodAmount)
+        {   
+            //construção é inicializada
+            isBeginning = true;
+            playerAnim.OnHammeringStarted();
+            houseSprite.color = startColor;
+            player.transform.position = point.position;
+            player.isPaused = true;
+            playerItems.currentWood -= woodAmount;
+        }
+
+        if(isBeginning)
+        {
+            timeCount += Time.deltaTime;
+
+            if(timeCount >= timeAmount)
+            {   //casa é finalizada
+                playerAnim.OnHammeringEnded();
+                houseSprite.color = endColor;
+                player.isPaused = false;
+                houseColl.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            detectingPlayer = false;
+        }
+    }
+}
