a04348928b5a4244cd723220b78af1f2ac57a374
AndreLuiz-LC <andrelzlemosc@gmail.com>
2024-07-24 16:49:57 -0300

    Lógica inimigo

--- PlayerAnim.cs
+++ PlayerAnim.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
+    [Header("Attack Settings")]
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask enemyLayer;
+
     private Player player;
     private Animator anim;
 
     private Casting cast;
+
+    private bool isHitting;
+    private float recoveryTime = 1.2f;
+    private float timeCount;
 
     void Start()
     {
@@ -21,51 +30,63 @@ public class PlayerAnim : MonoBehaviour
     {
         OnMove();
         OnRun();
+
+        timeCount += Time.deltaTime;
+
+        if (isHitting)
+        {
+            if (timeCount >= recoveryTime)
+            {
+                isHitting = false;
+                timeCount = 0f;
+            }
+        }
+
     }
 
     #region Movement
 
     void OnMove()
     {
-        if(player.direction.sqrMagnitude > 0)
+        if (player.direction.sqrMagnitude > 0)
         {
-            if(player.isRolling)
-        {
-            anim.SetTrigger("isRoll");
-        }
-        else
-        {
-            anim.SetInteger("transition", 1);
-        }
+            if (player.isRolling)
+            {
+                anim.SetTrigger("isRoll");
+            }
+            else
+            {
+                anim.SetInteger("transition", 1);
+            }
 
         }
-        
+
         else
         {
             anim.SetInteger("transition", 0);
         }
 
-        if(player.direction.x > 0)
+        if (player.direction.x > 0)
         {
             transform.eulerAngles = new Vector2(0, 0);
         }
-        
-        if(player.direction.x < 0)
+
+        if (player.direction.x < 0)
         {
             transform.eulerAngles = new Vector2(0, 180);
         }
 
-        if(player.isCutting)
+        if (player.isCutting)
         {
             anim.SetInteger("transition", 3);
         }
 
-        if(player.isDigging)
+        if (player.isDigging)
         {
             anim.SetInteger("transition", 4);
         }
 
-        if(player.isWatering)
+        if (player.isWatering)
         {
             anim.SetInteger("transition", 5);
         }
@@ -73,7 +94,7 @@ public class PlayerAnim : MonoBehaviour
 
     void OnRun()
     {
-        if(player.isRunning)
+        if (player.isRunning)
         {
             anim.SetInteger("transition", 2);
         }
@@ -81,6 +102,24 @@ public class PlayerAnim : MonoBehaviour
 
     #endregion
 
+    #region Attack
+
+    public void OnAttack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, enemyLayer);
+
+        if (hit != null)
+        {   //atacou o inimigo
+            hit.GetComponentInChildren<AnimationControl>().OnHit();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
+    }
+
+    #endregion Attack
     public void OnCastingStarted()
     {
         anim.SetTrigger("isCasting");
@@ -101,5 +140,14 @@ public class PlayerAnim : MonoBehaviour
     public void OnHammeringEnded()
     {
         anim.SetBool("hammering", false);
+    }
+
+    public void OnHit()
+    {
+        if (!isHitting)
+        {
+            anim.SetTrigger("hit");
+            isHitting = true;
+        }
     }
 }
